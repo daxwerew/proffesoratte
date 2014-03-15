6fc43228a4f47e60b7881b85c4493ef81317933f
DaxW <daxw@daxw.(none)>
2014-03-15 10:45:59 -0600

    practica de generar fechas

--- ctrls/grupoCtrl.php
+++ ctrls/grupoCtrl.php
@@ -16,35 +16,84 @@ class grupoCtrl{
 
 		switch( $_GET['accion'] ){
 			case 'alta':
+				$_VAR = $_GET;
 				//Validar datos
 				//Datos requeridos Nombre, Seccion, NRC, opcionales, academia, dias clase, horas por dia
-				if( !isset($_GET['nombre']) || !isset($_GET['seccion']) ||
-						!isset($_GET['nrc']) || !isset($_GET['evaluacion'])){
+				//nombre=a&seccion=D05&nrc=2&evaluacion[]=&dia_sem[]=1&dia_sem[]=3&fec_ini=25/02/2014&fec_fin=30/6/2014
+				if( !isset($_VAR['nombre']) || !isset($_VAR['seccion']) ||
+						!isset($_VAR['nrc']) || !isset($_VAR['evaluacion']) ||
+						!isset($_VAR['dia_sem']) || !isset($_VAR['fec_ini']) ||
+						!isset($_VAR['fec_fin'])
+						){
 					$error='no se indicaron datos suficientes';
 					require('vistas/error.php');
 					die;
 				}
 
-				$nombre = $_GET['nombre']);
-				$nrc  = $_GET['nrc'];
-				$seccion = $_GET['seccion'];
-				$evaluacion = $_GET['evaluacion'];
+				$nombre = $_VAR['nombre'];
+				$nrc  = $_VAR['nrc'];
+				$seccion = $_VAR['seccion'];
+				$evaluacion = $_VAR['evaluacion'];
+
+				$dia_sem = $_VAR['dia_sem'];
+				$fec_ini = $_VAR['fec_ini'];
+				$fec_fin = $_VAR['fec_fin'];
 
 
 				//seccion
-				if( !preg_match("/^([a-z]{2}\d{3}|[a-z]?\d{4})-[a-z]\d{2}$/i",$_GET['seccion']) ){
+				if( !preg_match("/^([a-z]{2}\d{3}|[a-z]?\d{4})-[a-z]\d{2}$/i",$_VAR['seccion']) ){
 					$error='seccion incorrecta';
 					require('vistas/error.php');
 					die;
 				}
 
 				//evaluacion
-				if( is_array($evaluacion) ){
+				/*if( is_array($evaluacion) ){
 					$error='evaluacion debe ser un arreglo de conceptos con su porcentaje';
+					require('vistas/error.php');
+					die;
+				}*/
+
+				//dias semana
+				if( !is_array($dia_sem) ){
+					$error='Dias semana debe ser arreglo';
 					require('vistas/error.php');
 					die;
 				}
 
+				foreach($dia_sem as $dsem ){
+					if( $dsem<1 && $dsem>6){
+						$error = 'Rango de días incorrecto';
+						require('vistas/error.php');
+						die;
+					}
+				}
+
+				//fechas
+				$fec_ini1 = explode("/",$fec_ini);
+				if( !checkdate($fec_ini1[1],$fec_ini1[0],$fec_ini1[2]) ){
+						$error = 'Fecha inicio no valida '.$fec_ini;
+						require('vistas/error.php');
+						die;
+				}
+				$fec_fin2 = explode("/",$fec_fin);
+				if( !checkdate($fec_fin2[1],$fec_fin2[0],$fec_fin2[2]) ){
+						$error = 'Fecha fin no valida';
+						require('vistas/error.php');
+						die;
+				}
+
+				$arre_diasem=array('Domingo','Lunes','Martes','Miercoles','Jueves','Viernes');
+
+				$un_dia = 86400;
+				$dia_ini = mktime(0,0,0,$fec_ini1[1],$fec_ini1[0],$fec_ini1[2]);
+				$dia_fin = mktime(0,0,0,$fec_fin2[1],$fec_fin2[0],$fec_fin2[2])+$un_dia ;
+
+				while( $dia_ini<$dia_fin ){
+					if( in_array(date('w',$dia_ini),$dia_sem) )
+						echo $arre_diasem[date('w',$dia_ini)],' ',date('d-m-Y',$dia_ini),'<br>';
+					$dia_ini += $un_dia;
+				}die;
 
 				//Llamada al modelo	
 				$estatus = $this->modelo->alta($nombre,$nrc,$seccion);
